00ec452ae3b6a0841b3d51bdeeb57ee137b03444
enginbektas <74172356+enginbektas@users.noreply.github.com>
2021-06-14 01:26:10 +0300

    dürüm

--- main.java
+++ main.java
@@ -11,10 +11,26 @@ public class main {
         array[1] = numberOfItems;
         array[2] = values;
         array[3] = capacities of knapsacks;
-        array[4] = knapsacks (sets of weights);
+        array[4] = knapsacks (sets of weights); (double array)
          */
-        Object array = Reader.read();
-        System.out.print("w");
+        Object array[] = Reader.read();
+
+        int size = (Integer)array[1];
+        int capacities[] = array[3];
+        int capacity = (Integer)array[3];
+        System.out.println(capacities);
+
+
+
+//
+//        Item arr[] = new Item[size];
+//        arr[0] = new Item(10, 2, 0);
+//        arr[1] = new Item(10, 4, 1);
+//        arr[2] = new Item(12, 6, 2);
+//        arr[3] = new Item(18, 9, 3);
+//
+//        solve(arr);
 
     }
+
 }
